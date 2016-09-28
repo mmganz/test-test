fafefd38400e36e8ede3bc85f8465136d417ce79
Meredith Ganz <2meredithganz@gmail.com>
2016-09-27 18:33:55 -0600

    push

--- app.js
+++ app.js
@@ -10,21 +10,39 @@ function drawSongs(songList){
   for (var i = 0; i < songList.length; i++) {
     var songs =songList[i]
     template+= ` <div class="template">
-      <div class="card">
-        <div class="card-block">
-          <h4 class="card-title">${songs.title}</h4>
-          <h6 class="card-subtitle text-muted">${songs.artist}</h6>
-        </div>
-        <img src="${songs.albumArt}">
-        <div class="card-block">
-          <p class="card-text">${songs.collection}</p>
-          <p class="card-text">$${songs.price}</p>
-          <audio controls>
-            <source id="audios" src="${songs.preview}">
-            </audio>
-        </div>
-      </div>
-    </div>`
+     <div class="mdl-card mdl-shadow--2dp mdl-card--horizontal">
+     <div class="mdl-card__media">
+     <img src="${songs.albumArt}">
+     </div>
+      <div class="mdl-card__title">
+      <h2 class="mdl-card__title-text">${songs.title}</h2>
+    </div>
+    <div class="mdl-card__supporting-text">
+    <p>${songs.artist}</p>
+    <p>${songs.collection}</p>
+    <p>$${songs.price}</p>
+    </div>
+</div>
+  </div>
+  </div>`
+
+
+    // template+= ` <div class="template">
+    //   <div class="card">
+    //     <div class="card-block">
+    //       <h4 class="card-title">${songs.title}</h4>
+    //       <h6 class="card-subtitle text-muted">${songs.artist}</h6>
+    //     </div>
+    //     <img src="${songs.albumArt}">
+    //     <div class="card-block">
+    //       <p class="card-text">${songs.collection}</p>
+    //       <p class="card-text">$${songs.price}</p>
+    //       <audio controls>
+    //         <source id="audios" src="${songs.preview}">
+    //         </audio>
+    //     </div>
+    //   </div>
+    // </div>`
   }
 
   document.addEventListener('play', function(e){
